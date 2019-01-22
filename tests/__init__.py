f00fd6d19446b9e1d4ef38e205d8dffb07a408ad
Lukas Zimmermann <luk.zim91@gmail.com>
2019-01-22 11:51:54 +0100

    Move response tests to train submodule

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,4 +8,4 @@ from .internal.builder import CnfBuilder1Tests, CnfBuilder2Tests
 from .internal.describe.property import EnvironmentVariablePropertyTests
 from .internal.describe.formula import CnfTests
 from .train.SimpleTrainDescribe import SimpleTrainDescribeTests
-from .response import RunResponseTests
+from tests.train.response import RunResponseTests
